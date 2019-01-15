e4bb5c6fd9c723078664b8ee79443f926d5a614d
kanehara <kanehara@umich.edu>
2019-01-14 18:21:13 -0600

    scaffolds webapp

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import theme from './theme'
+import Router from './Router'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: ${props => props.theme.fontFamily};
+    background-color: ${props => props.theme.colors.first};
+    color: ${props => props.theme.colors.second};
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <GlobalStyle />
+          <Router />
+        </React.Fragment>
+      </ThemeProvider>
     );
   }
 }
---
+++ webapp/src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  fontFamily: 'Fira Sans',
+  colors: {
+    first: '#292F36',
+    second: '#F7FFF7',
+    third: '#4ECDC4',
+    fourth: '#FF6B6B',
+    fifth: '#FFE66D'
+  }
+}
+
+module.exports = theme
