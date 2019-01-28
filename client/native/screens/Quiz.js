46e4fbeaedfcee4eb469a2b45e27d57bee09c4c7
kanehara <kanehara@umich.edu>
2019-01-27 20:24:50 -0600

    show questions

--- client/native/screens/Quiz.js
+++ client/native/screens/Quiz.js
@@ -4,15 +4,23 @@ import ScreenView from './ScreenView'
 import { connect } from 'react-redux'
 
 class QuizScreen extends React.Component {
+  get question () {
+    return this.props.questions[this.props.questionIndex]
+  }
+
   render() {
     return (
       <ScreenView>
-        <Text>{this.props.questions.length}</Text>
+        <Text>{this.question.category}</Text>
+        <Text>{this.question.question}</Text>
+        <Text>{this.props.questionIndex + 1} of {this.props.questions.length}</Text>
       </ScreenView>
     );
   }
 }
 
 export default connect(state => ({
-  questions: state.questions
+  finished: state.finished,
+  questions: state.questions,
+  questionIndex: state.questionIndex
 }))(QuizScreen)
